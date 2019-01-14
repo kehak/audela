631f8520d70a390770fee18e27e311cfb6bf9d3e
kehak <kehak@users.noreply.github.com>
2019-01-14 23:21:46 +0100

    Update buildindi.sh
    Resiliency fixes

--- buildindi.sh
+++ buildindi.sh
@@ -23,22 +23,26 @@ echo -e "\e[33m--> Installing dependencies...\e[0m"
 sudo apt install -y libnova-dev libcfitsio-dev libusb-1.0-0-dev zlib1g-dev libgsl-dev build-essential cmake git libjpeg-dev libcurl4-gnutls-dev libtiff-dev
 
 cd
-echo -e "\e[33m--> Retrieving indi v1.3\e[0m"
-wget -c https://github.com/indilib/indi/archive/v1.3.tar.gz
 
-echo -e "\e[33m--> Extracting archive\e[0m"
-tar xzf ./v1.3.tar.gz && cd ./indi-1.3
-mkdir -p build/libindi
+if [ ! -d "./indi-1.3" ]; then
+        echo -e "\e[33m--> Retrieving indi v1.3\e[0m"
+        wget -c https://github.com/indilib/indi/archive/v1.3.tar.gz
+
+        echo -e "\e[33m--> Extracting archive\e[0m"
+        tar xzf ./v1.3.tar.gz
+fi
+
+cd ./indi-1.3
+[ -d build/libindi ] || mkdir -p build/libindi
 cd build/libindi
+
 echo -e "\e[33m--> Building...\e[0m"
 cmake -DCMAKE_INSTALL_PREFIX=/usr/local -DCMAKE_BUILD_TYPE=Debug ../../libindi
 make -j4
 sudo make install
 
 echo -e "\e[33m--> Building drivers...\e[0m"
-
 for DRV in libapogee libfishcamp libsbig libqsi libqhy libfli fliusb generic-ccd; do build_driver $DRV; done
-
 for DRV in indi-aagcloudwatcher indi-apogee indi-asi indi-dsi indi-duino indi-eqmod indi-ffmv indi-fishcamp indi-fli indi-gige indi-gphoto indi-gpsd indi-maxdomeii indi-mi indi-nexstarevo indi-qhy indi-qsi indi-sbig indi-shelyak indi-spectracyber indi-ssag indi-sx indi-tess
 do
 	build_driver $DRV 
