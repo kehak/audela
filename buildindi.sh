5b1027bc0c94dabd3f047f846ee9f38b670643cf
kehak <kehak@users.noreply.github.com>
2019-01-15 16:34:03 +0100

    Update buildindi.sh
    Ajout des modifications de Benji

--- buildindi.sh
+++ buildindi.sh
@@ -1,10 +1,20 @@
 #!/bin/bash
 
+#--- Driver list:
+#- Selection:
+lib_list="libapogee libfishcamp libsbig libqsi libqhy libfli fliusb generic-ccd"
+driver_list="indi-apogee indi-asi indi-dsi indi-eqmod indi-fli indi-gphoto indi-mi indi-qhy indi-qsi indi-sbig indi-ssag indi-sx"
+
+#- Everything (do not edit)
+#api_list_all="libapogee libfishcamp libsbig libqsi libqhy libfli fliusb generic-ccd"
+#driver_list_all="indi-aagcloudwatcher indi-apogee indi-asi indi-dsi indi-duino indi-eqmod indi-ffmv indi-fishcamp indi-fli indi-gige indi-gphoto indi-gpsd indi-maxdomeii indi-mi indi-nexstarevo indi-qhy indi-qsi indi-sbig indi-shelyak indi-spectracyber indi-ssag indi-sx indi-tess"
+
+#--- Functions:
 build_driver() {
 	cd ..
 	mkdir $1
 	cd $1
-	cmake -DCMAKE_INSTALL_PREFIX=/usr/local -DCMAKE_BUILD_TYPE=Debug ../../3rdparty/$1
+	cmake -DCMAKE_INSTALL_PREFIX=/usr/local ../../3rdparty/$1
 	make -j4
 	sudo make install
 	sudo ldconfig
@@ -22,18 +32,12 @@ sudo apt remove -y libindi-dev libindi-data libindi-plugins
 echo -e "\e[33m--> Installing dependencies...\e[0m"
 sudo apt install -y libnova-dev libcfitsio-dev libusb-1.0-0-dev zlib1g-dev libgsl-dev build-essential cmake git libjpeg-dev libcurl4-gnutls-dev libtiff-dev
 
-cd
+echo -e "\e[33m--> Retrieving indi v1.3\e[0m"
+cd; wget -c https://github.com/indilib/indi/archive/v1.3.tar.gz
 
-if [ ! -d "./indi-1.3" ]; then
-        echo -e "\e[33m--> Retrieving indi v1.3\e[0m"
-        wget -c https://github.com/indilib/indi/archive/v1.3.tar.gz
-
-        echo -e "\e[33m--> Extracting archive\e[0m"
-        tar xzf ./v1.3.tar.gz
-fi
-
-cd ./indi-1.3
-[ -d build/libindi ] || mkdir -p build/libindi
+echo -e "\e[33m--> Extracting archive\e[0m"
+tar xzf ./v1.3.tar.gz && cd ./indi-1.3
+mkdir -p build/libindi
 cd build/libindi
 
 echo -e "\e[33m--> Building...\e[0m"
@@ -42,11 +46,8 @@ make -j4
 sudo make install
 
 echo -e "\e[33m--> Building drivers...\e[0m"
-for DRV in libapogee libfishcamp libsbig libqsi libqhy libfli fliusb generic-ccd; do build_driver $DRV; done
-for DRV in indi-aagcloudwatcher indi-apogee indi-asi indi-dsi indi-duino indi-eqmod indi-ffmv indi-fishcamp indi-fli indi-gige indi-gphoto indi-gpsd indi-maxdomeii indi-mi indi-nexstarevo indi-qhy indi-qsi indi-sbig indi-shelyak indi-spectracyber indi-ssag indi-sx indi-tess
-do
-	build_driver $DRV 
-done
+for DRV in $lib_list ; do build_driver $DRV; done
+for DRV in $driver_list; do build_driver $DRV; done
 
 echo -e "\e[33mInstalled drivers:\e[0m"
 ls /usr/local/bin/indi_*
